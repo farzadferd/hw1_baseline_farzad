7984f0d52b478b2795110ace086636c22584ac8e
fferdous <fferdous@umass.edu>
2025-03-04 17:26:24 -0500

    Created InputValidation file which handles amount and category inputs

---
+++ expense_tracker/src/InputValidation.java
@@ -0,0 +1,28 @@
+public class InputValidation {
+
+    public static boolean validAmount(double amount) {
+        return amount > 0 && amount < 1000;
+    }
+
+    public static boolean validCategory(String category) {
+        String[] validCategories = {"food", "travel", "bills", "entertainment", "other"};
+        
+        for (String validCateg : validCategories) {
+            if (validCateg.equalsIgnoreCase(category)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String validInputs(double amount, String category) {
+        if (!validAmount(amount)) {
+            return "Invalid Amount";
+        }
+        if (!validCategory(category)) {
+            return "Invalid Category";
+        }
+        return "Valid inputs";
+    }
+}
+
