0b184f00e7eea7b4f35805f99f1235c05cfc54ea
fferdous <fferdous@umass.edu>
2025-03-04 20:56:09 -0500

    Added comments for each method in Transaction file

--- expense_tracker/src/Transaction.java
+++ expense_tracker/src/Transaction.java
@@ -10,33 +10,34 @@ public class Transaction {
   private String category;
   private String timestamp;
 
-  public Transaction(double amount, String category) {
+  public Transaction(double amount, String category) { //Constructor that creates a new transaction with the given amount and category, automatically setting the timestamp.
+
     this.amount = amount;
     this.category = category;
     this.timestamp = generateTimestamp();
   }
 
-  public double getAmount() {
+  public double getAmount() { //Returns the amount of the transaction
     return amount;
   }
 
-  public void setAmount(double amount) {
+  public void setAmount(double amount) { //Sets the amount of the transaction.
     this.amount = amount;
   }
 
-  public String getCategory() {
+  public String getCategory() { //Returns the category of the transaction.
     return category;
   }
 
-  public void setCategory(String category) {
+  public void setCategory(String category) { //Sets the category of the transaction.
     this.category = category; 
   }
   
-  public String getTimestamp() {
+  public String getTimestamp() { //Returns the timestamp of the transaction.
     return timestamp;
   }
 
-  private String generateTimestamp() {
+  private String generateTimestamp() { //Generates and returns the current timestamp of the transaction
     SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");  
     return sdf.format(new Date());
   }
